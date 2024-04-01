a3a8002d918c51c69e0b807c4d9b6264ddecfbe2
iljaKorn <kornilov.ilja@rambler.ru>
2024-04-02 00:11:59 +0300

    add Profile modal component

--- src/components/supportComponents/Header.js
+++ src/components/supportComponents/Header.js
@@ -1,17 +1,27 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {Link} from "react-router-dom";
+import {useDispatch, useSelector} from "react-redux"
+import {unAuthorizeUser} from "../../store/userSlice";
 
 const Header = () => {
+
+    const [isOpen, setOpen] = useState(false);
+    const state = useSelector((state) => state);
+
+    const dispatch = useDispatch();
+    const unAuth = () => {
+        dispatch(unAuthorizeUser());
+    }
 
     return (
         <header className={"header"}>
             <div className={"logo"}>
-                <img className={"icon"} src={require('../../images/hermes.png')}
+                <img className={"logo-icon"} src={require('../../images/hermes.png')}
                      alt={"Error"}/>
                 <h1 className={"logo_name"}>Hermes</h1>
             </div>
 
-            <div className={"menu"}>
+            <nav className={"menu"}>
                 <div className={"menu_el"}>
                     <Link className="headLink" to="/">Главная</Link>
                 </div>
@@ -27,12 +37,24 @@ const Header = () => {
                 <div className={"menu_el"}>
                     <Link className="headLink" to="/cargo">Грузы</Link>
                 </div>
-
-            </div>
+            </nav>
 
             <div className={"profile"}>
-                <Link className="headLink" to="/"><img className={"icon"} src={require('../../images/profile.png')}
-                                                       alt={"Error"}/></Link>
+                <button className={"profile-button"} onClick={() => setOpen(!isOpen)}>
+                    <img className={"profile-icon"}
+                         src={require('../../images/profile.png')}
+                         alt={"Error"}/></button>
+                <nav className={`profile-main ${isOpen ? "active" : ""}`}>
+                    {
+                        state.user.isLogged ? <div>
+                            <h1>{state.user.user.username}</h1>
+                            <button onClick={unAuth}>Выйти</button>
+                        </div> : <div>
+                            <h1>Пожалуйста, зарегистрируйтесь</h1>
+                            <Link className="headLink" to="/auth">Войти</Link>
+                        </div>
+                    }
+                </nav>
             </div>
 
         </header>)
